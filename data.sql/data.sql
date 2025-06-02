4210842a717495dc66fe8a611a1c9831d29060ce
jonathan <jonathan@example.com>
2025-06-02 11:20:22 -0500

    nuevas inserciones

--- data.sql/data.sql
+++ data.sql/data.sql
@@ -1,3 +1,49 @@
+
+
+INSERT INTO usuarios (nombre, apellido, correo, tipo_usuario) VALUES
+('Juan', 'Pérez', 'juan.perez@email.com', 'estudiante'),
+('María', 'García', 'maria.garcia@email.com', 'estudiante'),
+('Carlos', 'Ramírez', 'carlos.ramirez@email.com', 'estudiante'),
+('Ana', 'Flores', 'ana.flores@email.com', 'estudiante'),
+('Luis', 'Torres', 'luis.torres@email.com', 'estudiante');
+
+
+
+
+
+INSERT INTO evaluaciones (titulo, tipo, area, fecha_inicio, fecha_fin, tiempo_minutos, id_curso) VALUES
+('Evaluación 1A', 'Parcial', 'Matemáticas', '2025-06-02 10:00:00', '2025-06-02 11:00:00', 60, 1),
+('Evaluación 1B', 'Parcial', 'Matemáticas', '2025-06-09 10:00:00', '2025-06-09 11:00:00', 60, 1),
+
+('Evaluación 2A', 'Parcial', 'Física', '2025-06-03 10:00:00', '2025-06-03 11:00:00', 60, 2),
+('Evaluación 2B', 'Parcial', 'Física', '2025-06-10 10:00:00', '2025-06-10 11:00:00', 60, 2),
+
+('Evaluación 3A', 'Parcial', 'Química', '2025-06-04 10:00:00', '2025-06-04 11:00:00', 60, 3),
+('Evaluación 3B', 'Parcial', 'Química', '2025-06-11 10:00:00', '2025-06-11 11:00:00', 60, 3),
+
+('Evaluación 4A', 'Parcial', 'Historia', '2025-06-05 10:00:00', '2025-06-05 11:00:00', 60, 4),
+('Evaluación 4B', 'Parcial', 'Historia', '2025-06-12 10:00:00', '2025-06-12 11:00:00', 60, 4),
+
+('Evaluación 5A', 'Parcial', 'Lenguaje', '2025-06-06 10:00:00', '2025-06-06 11:00:00', 60, 5),
+('Evaluación 5B', 'Parcial', 'Lenguaje', '2025-06-13 10:00:00', '2025-06-13 11:00:00', 60, 5),
+
+('Evaluación 6A', 'Parcial', 'Biología', '2025-06-07 10:00:00', '2025-06-07 11:00:00', 60, 6),
+('Evaluación 6B', 'Parcial', 'Biología', '2025-06-14 10:00:00', '2025-06-14 11:00:00', 60, 6);
+
+
+
+INSERT INTO cursos (nombre_curso) VALUES
+('Matemáticas'),
+('Física'),
+('Química'),
+('Historia'),
+('Lenguaje'),
+('Biología'),
+('Inglés');
+
+
+
+
 INSERT INTO evaluaciones (titulo, tipo, area, fecha_inicio, fecha_fin, tiempo_minutos) VALUES ('Evaluación 1', 'Parcial', 'Matemáticas', '2025-06-02 10:00:00', '2025-06-02 11:00:00', 60);
 INSERT INTO evaluaciones (titulo, tipo, area, fecha_inicio, fecha_fin, tiempo_minutos) VALUES ('Evaluación 2', 'Parcial', 'Matemáticas', '2025-06-03 10:00:00', '2025-06-03 11:00:00', 60);
 INSERT INTO evaluaciones (titulo, tipo, area, fecha_inicio, fecha_fin, tiempo_minutos) VALUES ('Evaluación 3', 'Parcial', 'Matemáticas', '2025-06-04 10:00:00', '2025-06-04 11:00:00', 60);
