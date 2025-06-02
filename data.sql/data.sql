176eab2b0841f458e9ae7ce4f6f2c32d02561857
jonathan <jonathan@example.com>
2025-06-02 11:25:01 -0500

    cambio de inserciones

--- data.sql/data.sql
+++ data.sql/data.sql
@@ -12,23 +12,16 @@ INSERT INTO usuarios (nombre, apellido, correo, tipo_usuario) VALUES
 
 
 INSERT INTO evaluaciones (titulo, tipo, area, fecha_inicio, fecha_fin, tiempo_minutos, id_curso) VALUES
-('Evaluación 1A', 'Parcial', 'Matemáticas', '2025-06-02 10:00:00', '2025-06-02 11:00:00', 60, 1),
-('Evaluación 1B', 'Parcial', 'Matemáticas', '2025-06-09 10:00:00', '2025-06-09 11:00:00', 60, 1),
-
-('Evaluación 2A', 'Parcial', 'Física', '2025-06-03 10:00:00', '2025-06-03 11:00:00', 60, 2),
-('Evaluación 2B', 'Parcial', 'Física', '2025-06-10 10:00:00', '2025-06-10 11:00:00', 60, 2),
-
-('Evaluación 3A', 'Parcial', 'Química', '2025-06-04 10:00:00', '2025-06-04 11:00:00', 60, 3),
-('Evaluación 3B', 'Parcial', 'Química', '2025-06-11 10:00:00', '2025-06-11 11:00:00', 60, 3),
-
-('Evaluación 4A', 'Parcial', 'Historia', '2025-06-05 10:00:00', '2025-06-05 11:00:00', 60, 4),
-('Evaluación 4B', 'Parcial', 'Historia', '2025-06-12 10:00:00', '2025-06-12 11:00:00', 60, 4),
-
-('Evaluación 5A', 'Parcial', 'Lenguaje', '2025-06-06 10:00:00', '2025-06-06 11:00:00', 60, 5),
-('Evaluación 5B', 'Parcial', 'Lenguaje', '2025-06-13 10:00:00', '2025-06-13 11:00:00', 60, 5),
-
-('Evaluación 6A', 'Parcial', 'Biología', '2025-06-07 10:00:00', '2025-06-07 11:00:00', 60, 6),
-('Evaluación 6B', 'Parcial', 'Biología', '2025-06-14 10:00:00', '2025-06-14 11:00:00', 60, 6);
+-- Evaluaciones Cerradas
+('Examen Diagnóstico', 'cerrada', 'Matemáticas', '2025-05-15 08:00:00', '2025-05-15 10:00:00', 120, 1),
+('Repaso General Física', 'cerrada', 'Física', '2025-05-18 09:00:00', '2025-05-18 11:00:00', 120, 2),
+('Simulacro Historia', 'cerrada', 'Historia', '2025-05-22 08:30:00', '2025-05-22 10:30:00', 120, 4),
+-- Evaluaciones Parciales
+('Evaluación Parcial A1', 'Parcial', 'Lenguaje', '2025-06-10 10:00:00', '2025-06-10 11:00:00', 60, 5),
+('Evaluación Parcial B1', 'Parcial', 'Biología', '2025-06-11 10:00:00', '2025-06-11 11:00:00', 60, 6),
+('Evaluación Parcial C1', 'Parcial', 'Química', '2025-06-12 10:00:00', '2025-06-12 11:00:00', 60, 3),
+('Evaluación Parcial D1', 'Parcial', 'Razonamiento', '2025-06-13 10:00:00', '2025-06-13 11:00:00', 60, 2),
+('Evaluación Parcial E1', 'Parcial', 'Comunicación', '2025-06-14 10:00:00', '2025-06-14 11:00:00', 60, 1);
 
 
 
